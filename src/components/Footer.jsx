94fdcf1fe36d2faf97aeef49612611a40e6b53b9
Amaryllis Pittin <amaryllispittin@gmail.com>
2024-05-23 11:39:58 +0200

    travail sur le responsive et début d'intégration du burger menu

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,55 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 const Footer = () => {
-    const [formData, setFormData] = useState({
-        name: '',
-        email: '',
-        but: 'Collaboration professionnelle',
-        message: ''
-    });
-
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({
-            ...formData,
-            [name]: value
-        });
-    };
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            const response = await fetch('http://localhost:3001/send-email', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(formData)
-            });
-            if (response.ok) {
-                const responseData = await response.text();
-                alert(responseData);
-            } else {
-                alert('Une erreur est survenue lors de l\'envoi du message.');
-            }
-        } catch (error) {
-            console.error(error);
-            alert('Une erreur est survenue lors de l\'envoi du message.');
-        }
-    };
-
     return (
         <footer className="footer" id="contact">
             <h2 className="footer__h2">Me contacter</h2>
-            <form className="footer-form" onSubmit={handleSubmit}>
+            <form className="footer-form" action="mailto:amaryllispittin@gmail.com" method="post" encType="text/plain">
                 <label htmlFor="name">Nom</label>
-                <input type="text" id="name" name="name" placeholder="Nom" value={formData.name} onChange={handleChange} required />
+                <input type="text" id="name" name="name" placeholder="Nom" required />
                 
                 <label htmlFor="email">E-mail</label>
-                <input type="email" id="email" name="email" placeholder="Entrez votre adresse e-mail" value={formData.email} onChange={handleChange} required />
+                <input type="email" id="email" name="email" placeholder="Entrez votre adresse e-mail" required />
                 
                 <label htmlFor="but-select">Objet</label>
-                <select name="but" id="but-select" value={formData.but} onChange={handleChange} required>
+                <select name="but" id="but-select" required>
                     <option value="Collaboration professionnelle">Collaboration professionnelle</option>
                     <option value="Aide technique">Aide technique</option>
                     <option value="Proposition de projet">Proposition de projet</option>
@@ -58,7 +21,7 @@ const Footer = () => {
                 </select>
                 
                 <label htmlFor="message">Message</label>
-                <textarea id="message" name="message" rows="5" cols="33" placeholder="Entrez votre message" value={formData.message} onChange={handleChange} required></textarea>
+                <textarea id="message" name="message" rows="5" cols="33" placeholder="Entrez votre message" required></textarea>
                 
                 <input className="input-submit" type="submit" value="Envoyer" />
             </form>
@@ -68,3 +31,4 @@ const Footer = () => {
 };
 
 export default Footer;
+
--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,35 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import { HashLink as Link } from 'react-router-hash-link';
 
 const Topbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <div className="topbar-container">
-      <ul className="topbar">
-        <li>
+      <ul className={`topbar ${showMenu ? "show-menu" : ""}`}>
+        <li className="header-link">
           <Link className="topbar-link" smooth to="/#headerBanner">Accueil</Link>
         </li>
-        <li>
+        <li className="hide-on-mobile">
           <Link className="topbar-link" smooth to="/#about">Présentation</Link>
         </li>
-        <li>
+        <li className="hide-on-mobile">
           <Link className="topbar-link" smooth to="/#services">Services</Link>
         </li>
-        <li>
+        <li className="hide-on-mobile">
           <Link className="topbar-link" smooth to="/#realisations">Réalisations</Link>
         </li>
-        <li>
+        <li className="hide-on-mobile">
           <Link className="topbar-link" smooth to="/#competences">Compétences</Link>
         </li>
-        <li>
+        <li className="hide-on-mobile">
           <Link className="topbar-link" smooth to="/#formation">Formations et CV</Link>
         </li>
-        <li>
+        <li className="hide-on-mobile">
           <Link className="topbar-link" smooth to="/#contact">Contact</Link>
         </li>
+        <li className="menu-icon" onClick={toggleMenu}>
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </li>
+        {showMenu && (
+          <div className="dropdown-menu">
+            {/* Mettez ici vos liens supplémentaires pour le menu déroulant */}
+            <Link className="dropdown-link" smooth to="/#about">Présentation</Link>
+            <Link className="dropdown-link" smooth to="/#services">Services</Link>
+            <Link className="dropdown-link" smooth to="/#realisations">Réalisations</Link>
+            <Link className="dropdown-link" smooth to="/#competences">Compétences</Link>
+            <Link className="dropdown-link" smooth to="/#formation">Formations et CV</Link>
+            <Link className="dropdown-link" smooth to="/#contact">Contact</Link>
+          </div>
+        )}
       </ul>
     </div>
   );
 };
 
 export default Topbar;
+
+
 
