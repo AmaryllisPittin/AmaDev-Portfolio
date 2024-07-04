166d57b76c69dc74bf2a725d5eb25e82e8b34f83
Amaryllis Pittin <amaryllispittin@gmail.com>
2024-07-04 15:40:55 +0200

    accès au code githhub corrigé

--- src/pages/PageRealisation.jsx
+++ src/pages/PageRealisation.jsx
@@ -12,17 +12,22 @@ const PageRealisation = () => {
     return <NotFound />;
   }
 
+  const isGitHubLink = item.url.includes("github.com");
+  const linkText = isGitHubLink ? "Voir le code" : "Visiter le site";
+
   return (
     <div className="realisation-container">
       <h1 className="realisation-title">{item.title}</h1>
-      <a href={item.url} target="blank" className="realisation-link">Visiter le site</a>
+      <a href={item.url} target="_blank" rel="noopener noreferrer" className="realisation-link">{linkText}</a>
       <Tags />
-          <p className="description-p">{item.description1}</p>
-          <p className="description-p">{item.description2}</p>
-          <p className="description-p last-p">{item.description3}</p>
-      </div>
+      <p className="description-p">{item.description1}</p>
+      <p className="description-p">{item.description2}</p>
+      <p className="description-p last-p">{item.description3}</p>
+    </div>
   );
 };
 
 export default PageRealisation;
+
+
 
